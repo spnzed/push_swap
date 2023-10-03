31432c7d4b1fd75637616ddeca518f0369a6bf49
Aaron Espinosa <aaespino@car2s6.42barcelona.com>
2023-10-03 14:38:36 +0200

    Fresh start

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaronespinosa <aaronespinosa@student.42    +#+  +:+       +#+        */
+/*   By: aaespino <aaespino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/13 17:17:11 by aaespino          #+#    #+#             */
-/*   Updated: 2023/06/02 11:56:15 by aaronespino      ###   ########.fr       */
+/*   Updated: 2023/10/02 14:01:57 by aaespino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,13 +58,18 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_itoa(int n);
 char	**ft_split(char const *s, char c);
 int		ft_strrncmp(const char *str, const char *str2, size_t n);
-void    ft_lstclear(t_list **lst, void(*del)(void *));
-int		ft_lstpos(t_list *lst, int *value);
-void    ft_lstadd_back(t_list **lst, t_list *new);
+
+t_list  *ft_lstnew(void *content);
 void    ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
-t_list  *ft_lstnew(void *content);
 t_list  *ft_lstlast(t_list *lst);
+void    ft_lstadd_back(t_list **lst, t_list *new);
+void    ft_lstdelone(t_list *lst, void (*del)(void *));
+void    ft_lstclear(t_list **lst, void(*del)(void *));
+void    ft_lstiter(t_list **lst, void(*f)(void *));
+t_list	ft_lstmap(t_list *lst, void(*f)(void *), void(*del)(void *));
+
+int		ft_lstpos(t_list *lst, int *value);
 void    *ft_delete(void *p);
 
 #endif
